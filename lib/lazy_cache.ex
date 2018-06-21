2c0e59aa7918be64e6a11c1b15cfb1ae6d3ea6e2
Arturo <arturo@trovit.com>
2018-06-21 12:08:10 +0200

    Schedule periodic works for clearing db of expired items

--- lib/lazy_cache.ex
+++ lib/lazy_cache.ex
@@ -1,18 +1,40 @@
 defmodule LazyCache do
+  use GenServer
+
   @moduledoc """
   Documentation for LazyCache.
   """
 
   @doc """
-  Hello world.
+  Start scheduling the works for clearing the DB.
 
   ## Examples
 
-      iex> LazyCache.hello
-      :world
+      iex> LazyCache.start
+      {:ok, #PID<0.112.0>}
 
   """
-  def hello do
-    :world
+  def start do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  @impl true
+  def init(state) do
+    # Schedule work to be performed on start
+    schedule_work()
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:work, state) do
+    # Do the desired work here
+    # Reschedule once more
+    schedule_work()
+    {:noreply, state}
+  end
+
+  defp schedule_work() do
+    # In 2 seconds
+    Process.send_after(self(), :work, 2 * 1000)
   end
 end
