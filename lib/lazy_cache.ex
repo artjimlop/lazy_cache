f3d576e00347e7ccb9a878b665edc061c2a26ad7
Arturo <arturo@trovit.com>
2018-06-21 15:58:11 +0200

    Basic cache operations :skull:

--- lib/lazy_cache.ex
+++ lib/lazy_cache.ex
@@ -18,10 +18,75 @@ defmodule LazyCache do
     GenServer.start_link(__MODULE__, %{})
   end
 
+  def insert(key, value, keepAliveInMillis) do
+    is_inserted = :ets.insert(:buckets_registry, {key, value, keepAliveInMillis})
+
+    :ets.lookup(:key_set, :keys)[:keys]
+    |> check_if_key_already_in_keyset(key)
+
+    if is_inserted do
+      append_to_keyset(:ets.lookup(:key_set, :keys)[:keys], key)
+    end
+
+    is_inserted
+  end
+
+  def lookup(key) do
+    :ets.lookup(:buckets_registry, key)
+  end
+
+  def delete(key) do
+    is_deleted = :ets.delete(:buckets_registry, key)
+
+    if is_deleted do
+      delete_from_keyset(:ets.lookup(:key_set, :keys)[:keys], key)
+    end
+
+    is_deleted
+  end
+
+  def size() do
+    length(:ets.lookup(:key_set, :keys)[:keys])
+  end
+
+  def clear() do
+    if size() == 0 do
+      false
+    else
+      for key <- :ets.lookup(:key_set, :keys)[:keys], do: delete(key)
+      true
+    end
+  end
+
+  defp update_key_set(key_set) do
+    :ets.delete(:key_set, :keys)
+    :ets.insert(:key_set, {:keys, key_set})
+  end
+
+  defp check_if_key_already_in_keyset(key_set, key) do
+    if Enum.member?(key_set, key) do
+      delete_from_keyset(key_set, key)
+    end
+  end
+
+  defp delete_from_keyset(key_set, key) do
+    List.delete(key_set, key)
+    |> update_key_set()
+  end
+
+  defp append_to_keyset(key_set, key) do
+    (key_set ++ [key])
+    |> update_key_set()
+  end
+
   @impl true
   def init(state) do
     # Schedule work to be performed on start
     schedule_work()
+    # Create table
+    :ets.new(:buckets_registry, [:set, :public, :named_table])
+    :ets.new(:key_set, [:set, :public, :named_table])
+    :ets.insert(:key_set, {:keys, []})
     {:ok, state}
   end
 
