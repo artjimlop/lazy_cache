0d835e837a9fc383a31507b5e1bc1cb4b559cbac
Arturo <arturo@trovit.com>
2018-06-22 11:17:53 +0200

    v 0.1.0

--- mix.exs
+++ mix.exs
@@ -1,14 +1,21 @@
 defmodule LazyCache.MixProject do
   use Mix.Project
 
+  @maintainers [
+    "Arturo Jiménez"
+  ]
+
   def project do
     [
       app: :lazy_cache,
       version: "0.1.0",
       elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      maintainers: @maintainers,
       deps: deps(),
-
+      description: description(),
+      package: package(),
       # Docs
       name: "LazyCache",
       source_url: "https://github.com/artjimlop/lazy_cache",
@@ -28,5 +35,21 @@ defmodule LazyCache.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [{:ex_doc, "~> 0.16", only: :dev, runtime: false}]
+  end
+
+  defp description() do
+    "A memcached-like Elixir cache."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "lazy_cache",
+      maintainers: @maintainers,
+      # These are the default files included in the package
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      licenses: ["GNU General Public License v3.0"],
+      links: %{"GitHub" => "https://github.com/artjimlop/lazy_cache"}
+    ]
   end
 end
